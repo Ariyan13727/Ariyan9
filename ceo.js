1ecfdeab86ed2944873b059d18e9177928328b99
Ariyan13727 <ariyankhan1262552@gmail.com>
2026-06-06 10:34:36 +0300

    Create ceo.js

---
+++ ceo.js
@@ -0,0 +1,16 @@
+module.exports = {
+  CEO: {
+    name: "Ip-Ariyan",
+    bio: `
+⎯ 國𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮𝐥𝐚𝐢𝐤𝐮𝐦 ⎯ 🩷🌷
+
+Name: •° Ip-Ariyan °• 🙃
+Address: Tangail
+
+😹 Poribarer Boro Soytan 💯
+😎 Tar Thekeo Boro Porichoy
+
+☪️ Ami Ekjon Muslim Parivaarer Santan 🥰🤍
+    `
+  }
+};
