b7ab44c3659398f42ea73ae48e7eee486bec9478
Ariyan13727 <ariyankhan1262552@gmail.com>
2026-06-06 11:01:29 +0300

    Create ceoinfo.js

---
+++ Script/commands/ceoinfo.js
@@ -0,0 +1,27 @@
+module.exports = {
+	config: {
+		name: "ceoinfo",
+		version: "1.0",
+		author: "Ip-Ariyan",
+		countDown: 5,
+		role: 0,
+		shortDescription: "CEO Information",
+		longDescription: "Show CEO Information",
+		category: "info"
+	},
+
+	onStart: async function ({ message }) {
+		return message.reply(`
+⎯ 國𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮𝐥𝐚𝐢𝐤𝐮𝐦⎯ 🩷🌷
+
+👤 Name: •° Ip-Ariyan °• 🙃
+🏠 Basha: Tangail
+
+😹 Poribarer Boro Soytan 💯
+
+☪️ Tar Thekeo Boro Porichoy
+
+🥰 Ami Ekjon Muslim Parivaarer Santan
+		`);
+	}
+};
